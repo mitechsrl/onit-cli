56d7fe1257f3ca2e8c29e197710f3fda48ab0476
ivanvaccarimitech <ivan.vaccari@mitechsrl.it>
2020-11-25 17:30:48 +0100

    Updates serve

--- bin/serve/_src/lib/webpack.js
+++ bin/serve/_src/lib/webpack.js
@@ -6,21 +6,43 @@ const find = require('find');
 const onitFileLoader = require('../../../../lib/onitFileLoader');
 const webpackUtils = require('../../../../lib/webpack/utils');
 
+const wepbackParallel = require('parallel-webpack').run
 
-module.exports.start = async (logger, onitRunFile, mainOnitBuildFile) => {
+module.exports.start = async (logger, cwdOnitRunFile, cwdOnitBuildFile) => {
 
     // load the default config
-    let  webpackConfig = require('../../../../configFiles/serve/webpack.config');
+    let  webpackConfigFactory = require('../../../../configFiles/serve/webpack.config');
 
-    // add dynamic entry points to the webpack config
-    webpackConfig.entry = webpackUtils.searchEntryPoints(onitRunFile);
+    let webpackConfigs = [];
 
-    const onitBuildWebpackData = await webpackUtils.getOnitBuildFilesData(onitRunFile, mainOnitBuildFile);
+    // create a webpack config for the current directory and
+    // add dynamic entry points to the webpack config for the current directory
+    const cwdWebpackConfig = webpackConfigFactory(process.cwd());    
+    cwdWebpackConfig.entry = webpackUtils.searchEntryPoints(process.cwd());
+    webpackConfigs.push(cwdWebpackConfig);
+
+    // create one webpack config for each one of the components loaded in dev environment
+    (cwdOnitRunFile.json.loadComponents || [])
+        .filter(c => c.enabled) // skip disabled ones
+        .filter(c => c.path.indexOf('node_modules')<0) // still never search stuff in ode_modules
+        .forEach(component => {
+            const componentPath = path.resolve(process.cwd(), component.path);
+            // create a webpack config for the current directory and
+            // add dynamic entry points to the webpack config for the current directory
+            const webpackConfig = webpackConfigFactory(componentPath);    
+            webpackConfig.entry = webpackUtils.searchEntryPoints(componentPath);
+            webpackConfigs.push(webpackConfig);
+        })
+    console.log(webpackConfigs);
+
+
+    
+    /*const onitBuildWebpackData = await webpackUtils.getOnitBuildFilesData(onitRunFile, mainOnitBuildFile);
 
     webpackConfig = _.mergeWith(webpackConfig, onitBuildWebpackData, webpackUtils.webpackMergeFn);
-
-    console.log(webpackConfig);
     
+    */
+
     // watcher callback
     const watcherCallback = (err, stats) => {
         // do we had internal errors?
@@ -46,15 +68,14 @@ module.exports.start = async (logger, onitRunFile, mainOnitBuildFile) => {
     return new Promise(resolve =>{
 
         // create a compiler based on the config
-        const compiler = webpack(webpackConfig);
+        const compiler = wepbackParallel(webpackConfigs,
+            {
+                maxRetries: 1,
+                stats: true, // defaults to false
+                maxConcurrentWorkers: 2 // use 2 workers
+            }, watcherCallback);
 
-        // start the watcher!
-        const watcher = compiler.watch({
-            aggregateTimeout: 500,
-            ignored: ['files/**/*.js', 'node_modules/**']
-        }, watcherCallback);
-
-
+        console.log(compiler);
         process.on('SIGINT',() =>{
             logger.warn("Stop webpack watcher...");
             watcher.close(() => {
--- configFiles/serve/webpack.config.js
+++ configFiles/serve/webpack.config.js
@@ -6,88 +6,96 @@ const babelRcJs = require('./babel.config');
  * Webpack standard config. Some values may be changed at runtime (especially entry points and/or mode)
  */
  
-module.exports = {
-    mode: 'development',
-    context: process.cwd(),
-
-    // see https://webpack.js.org/configuration/devtool/ for available devtools
-    devtool:'source-map',
+module.exports = (context) => { // context usually is process.env()
     
-    // babel files loader
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [{
-                    loader: require.resolve('style-loader'),
-                    options: { injectType: 'singletonStyleTag' }
+    return {
+        mode: 'development',
+        context: context,
+
+        watch:true, 
+        watchOptions: {
+            aggregateTimeout: 500,
+            ignored: ['node_modules/**']
+        },
+
+        // see https://webpack.js.org/configuration/devtool/ for available devtools
+        devtool:'source-map',
+        
+        // babel files loader
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [{
+                        loader: require.resolve('style-loader'),
+                        options: { injectType: 'singletonStyleTag' }
+                    },
+                    {
+                        loader: require.resolve('css-loader')
+                    }]
                 },
                 {
-                    loader: require.resolve('css-loader')
-                }]
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: [
-                    {
-                        loader: require.resolve('file-loader'),
-                        options: { 
-                            name: (resourcePath) =>{
-                                const dirName = path.dirname(resourcePath);
-                                
-                                // use min also for non min. We don't care about it in dev mode, but we can leave the min import somewhere and don't bother it when
-                                // production build is compiled
-                                const baseName = path.basename(resourcePath).replace(/(sass|scss)$/, "min.css"); 
-                                const finalName = path.relative(process.cwd(), path.join(dirName, baseName));
-                                
-                                return finalName;
-                                //was '[path][name].css'
+                    test: /\.s[ac]ss$/,
+                    use: [
+                        {
+                            loader: require.resolve('file-loader'),
+                            options: { 
+                                name: (resourcePath) =>{
+                                    const dirName = path.dirname(resourcePath);
+                                    
+                                    // use min also for non min. We don't care about it in dev mode, but we can leave the min import somewhere and don't bother it when
+                                    // production build is compiled
+                                    const baseName = path.basename(resourcePath).replace(/(sass|scss)$/, "min.css"); 
+                                    const finalName = path.relative(context, path.join(dirName, baseName));
+                                    
+                                    return finalName;
+                                    //was '[path][name].css'
+                                }
                             }
-                        }
-                    },
-                    require.resolve('sass-loader')
-                ]
-            },
-            {
-                test: /\.jsx$/,
-                use: {
-                    loader: require.resolve('babel-loader'),
-                    options: babelRcJs
+                        },
+                        require.resolve('sass-loader')
+                    ]
+                },
+                {
+                    test: /\.jsx$/,
+                    use: {
+                        loader: require.resolve('babel-loader'),
+                        options: babelRcJs
+                    }
                 }
-            }
-        ]
-    },
+            ]
+        },
 
-    // do not split chunks as now. Shared libs are managed with externals, and entry points will be packed as single files
-    // Maybe in future this will be reviewed but as now use this config
-    optimization: {
-        splitChunks: false
-    },
+        // do not split chunks as now. Shared libs are managed with externals, and entry points will be packed as single files
+        // Maybe in future this will be reviewed but as now use this config
+        optimization: {
+            splitChunks: false
+        },
 
-    // resolves will be populated dynamically from project dependency files (onitbuild.config.json's)
-    resolve: {
-    },
+        // resolves will be populated dynamically from project dependency files (onitbuild.config.json's)
+        resolve: {
+        },
 
-    // see https://webpack.js.org/configuration/output/
-    output: {
-        path: process.cwd(),
-        filename: '[name].js', //or '[name].min.js'
-    },
+        // see https://webpack.js.org/configuration/output/
+        output: {
+            path: context,
+            filename: '[name].js', //or '[name].min.js'
+        },
 
-    // these libs are loaded manually in the browser (some of them are standard, some others are custom made)
-    externals:{
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        moment: 'moment',
-        'prop-types': 'PropTypes',
-        utility: 'utility',
-        d3: 'd3',
-        swal2b: 'Swal2b',
-        lodash: '_',
-        toastr: 'toastr'
-    },
+        // these libs are loaded manually in the browser (some of them are standard, some others are custom made)
+        externals:{
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            moment: 'moment',
+            'prop-types': 'PropTypes',
+            utility: 'utility',
+            d3: 'd3',
+            swal2b: 'Swal2b',
+            lodash: '_',
+            toastr: 'toastr'
+        },
 
-    // see https://webpack.js.org/configuration/stats/#stats-presets
-    stats:'verbose'
-
+        // see https://webpack.js.org/configuration/stats/#stats-presets
+        stats:'verbose'
+    }
 };
