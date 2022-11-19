203a4f4480e213edbdb320ae8f2e2cae25e97662
ivanvaccari <ivan.vaccari@mitechsrl.it>
2022-11-19 14:08:28 +0100

    Removed unwanted files

--- src/lib/compatibilityParams.ts
+++
@@ -1,9 +0,0 @@
-['-exit'].forEach(c => {
-    process.argv.forEach((p,index)=> {
-        if (p === c ) process.argv[index] = '-'+process.argv[index];
-    });
-});
-
-export function getParams(){
-    return process.argv
-} 
