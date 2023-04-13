c973dc91e1b7790743635a05bee4466e7d2dc56a
AlessandroPolcini <alessandro.polcini@mitechsrl.it>
2023-04-13 17:14:02 +0200

    Updated stuff

--- dist/bin/create/app/_lib/replaceValues.js
+++ dist/bin/create/app/_lib/replaceValues.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.replaceValues = void 0;
 const fs_1 = require("fs");
-const lodash_1 = require("lodash");
 const promises_1 = require("fs/promises");
 const path_1 = require("path");
 async function getFiles(dir) {
@@ -22,22 +21,24 @@ async function getFiles(dir) {
 async function replaceValues(cwd, answers) {
     const originalCwd = process.cwd();
     process.chdir(cwd);
-    const replacements = [
-        // { find: '@mitech/onit-next-example-webcomponent', replace: answers.appName },
-        { find: 'OnitExampleWebComponent', replace: answers.componentClassName },
-        { find: 'ONIT_EXAMPLE_WEB_COMPONENT', replace: answers.componentNameExport },
-        { find: 'exampleWebComponent', replace: answers.componentClassNameShortCamelCase },
-        { find: 'ExampleWebComponent', replace: (0, lodash_1.upperFirst)(answers.componentClassNameShortCamelCase) },
-        { find: 'Onit example web component', replace: answers.appExtendedName },
-        { find: 'onit-next-example-webcomponent', replace: answers.appNameWithoutScope }
-    ];
+    const replacements = answers.repo.replacer(answers);
     const files = [
         // join(process.cwd(), 'package.json'),
         (0, path_1.join)(process.cwd(), 'README.md'),
         ...await getFiles((0, path_1.join)(process.cwd(), './src'))
     ];
+    const nextJsConfig = (0, path_1.join)(process.cwd(), 'next.config.js');
+    if ((0, fs_1.existsSync)(nextJsConfig))
+        files.push(nextJsConfig);
     files.forEach(file => {
-        let content = (0, fs_1.readFileSync)(file).toString();
+        let content = '';
+        try {
+            content = (0, fs_1.readFileSync)(file).toString();
+        }
+        catch (e) {
+            console.warn(`[WARNING] File "${file}" not found. Skipping find-replace operations for this file...`);
+            return;
+        }
         const original = content;
         replacements.forEach(r => {
             content = content.replace(new RegExp(r.find, 'g'), r.replace);
