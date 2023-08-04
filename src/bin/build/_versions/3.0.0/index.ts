d2d7877c2ab4d72a0aa4f45b87c1251bacddfc32
ivanvaccari <ivan.vaccari@mitechsrl.it>
2023-08-04 16:32:57 +0200

    Added $_PACKAGE_NAME_NO_SCOPE var to before/after steps

--- src/bin/build/_versions/3.0.0/index.ts
+++ src/bin/build/_versions/3.0.0/index.ts
@@ -83,6 +83,8 @@ export default async function build(onitConfigFile: OnitConfigFile, argv: yargs.
     // prepare some vars for next steps
     const vars = {
         $_PROJECT_DIR: process.cwd(),
+        // NOTE: fix wrong replacement when one var name is included in another one 
+        $_PACKAGE_NAME_NO_SCOPE: cwdPackageJson.name.replace(/(@.*\/)([^/]+)$/,'$2'),
         $_PACKAGE_NAME: cwdPackageJson.name,
         $_PACKAGE_VERSION: cwdPackageJson.version
     };
